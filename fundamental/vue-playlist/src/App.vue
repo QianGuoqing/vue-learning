6e91bd490d377c8dd08ef01f8c148948612cadcb
QianGuoqing <qian_guoqing@yeah.net>
2017-12-21 16:09:29 +0800

    add vue-cli with components

---
+++ fundamental/vue-playlist/src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="app">
+    <app-header :title="title"></app-header>
+    <users :users="users"></users>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import users from './components/users'
+import header from './components/header'
+import footer from './components/footer'
+export default {
+  name: 'app',
+  components: {
+    users,
+    'app-header': header,
+    'app-footer': footer
+  },
+  data() {
+    return {
+      title: '传递的是一个值(number, string, boolean)',
+      users: [
+          {
+            name: 'Henry',
+            position: 'Web开发',
+            show: false
+          },
+          {
+            name: 'Henry',
+            position: 'Web开发',
+            show: false
+          },
+          {
+            name: 'Henry',
+            position: 'Web开发',
+            show: false
+          },
+          {
+            name: 'Henry',
+            position: 'Web开发',
+            show: false
+          },
+          {
+            name: 'Henry',
+            position: 'Web开发',
+            show: false
+          },
+          {
+            name: 'Henry',
+            position: 'Web开发',
+            show: false
+          },
+          {
+            name: 'Henry',
+            position: 'Web开发',
+            show: false
+          },
+          {
+            name: 'Henry',
+            position: 'Web开发',
+            show: false
+          }
+        ]
+    }
+  },
+}
+</script>
+
+<style>
+h1 {
+  color: purple;
+}
+</style>
---
+++ fundamental/vue-playlist/src/components/users.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="users">
+    <h1>hello users</h1>
+    <ul>
+      <li v-for="(user, index) in users" :key="index" @click="user.show = !user.show">
+        <h2>{{ user.name }}</h2>
+        <h3 v-show="user.show">{{ user.position }}</h3>
+      </li>
+    </ul>
+
+    <button @click="deleteUser">删除</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'users',
+    props: {
+      users: {
+        type: Array,
+        default: []
+      },
+    },
+    data() {
+      return {
+        
+      }
+    },
+    methods: {
+      deleteUser() {
+        
+      }
+    },
+  }
+</script>
+
+<style scoped>
+.users {
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 20px;
+  box-sizing: border-box;
+}
+
+ul {
+  display: flex;
+  flex-wrap: wrap;
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  flex-grow: 1;
+  flex-basis: 200px;
+  text-align: center;
+  padding: 30px;
+  border: 1px solid #222;
+}
+</style>
